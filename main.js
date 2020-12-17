871995998202ee429f5e89670920e0ea746089dd
Eamon McHugh <eamonmchugh2@gmail.com>
2020-12-17 00:18:58 -0500

    basic file created, nav created, input fields created for time lapse calc

---
+++ main.js
@@ -0,0 +1,10 @@
+//nav init 
+$(document).ready(function(){
+    $('#slide-out').sidenav();
+  });
+     
+
+//   Input init
+$(document).ready(function() {
+    $('input#input_text, textarea#textarea2').characterCounter();
+  });
