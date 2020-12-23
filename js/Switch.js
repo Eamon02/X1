d633b5c46d784b11eec31b899183510644e7da75
Eamon McHugh <eamonmchugh2@gmail.com>
2020-12-22 19:29:44 -0500

    got switch.js to work using hide class

--- js/Switch.js
+++ js/Switch.js
@@ -3,30 +3,46 @@ const moon = document.querySelector(".moon");
 const sun = document.querySelector(".sun")
 
 // on change of checkbox, executes functions
-checkbox.addEventListener("change", AstroStyle, toggleAstro)
+// checkbox.addEventListener("change", AstroStyle, toggleAstro)
 // checkbox.addEventListener("change", AstroStyle(), toggleAstro())
 
     //Change from TL to Astro TL
+// function AstroStyle(){
+//     moon.classList.toggle('dark');
+//     sun.classList.toggle('dark');
+// };
+
+//Switch IF statment
+document.addEventListener('DOMContentLoaded', function () {
+    var checkbox = document.querySelector('input[type="checkbox"]');
+  
+    checkbox.addEventListener('change', function () {
+      if (!checkbox.checked) {
+        // do this
+        AstroStyle()
+        AstroToggle()
+        $('.tl-header').text("Time Lapse Calculator")
+        $('#calc1').removeClass('hide')
+        $('#calc2').addClass('hide')
+
+        console.log('Checked');
+      } else {
+        // do that
+        console.log('Not checked');
+        $('.tl-header').text("Banana")
+        $('#calc1').addClass('hide')
+        $('#calc2').removeClass('hide')
+      }
+    });
+  });
+
+    // Change from TL to Astro TL
 function AstroStyle(){
     moon.classList.toggle('dark');
     sun.classList.toggle('dark');
 };
 
 //Changes HTML content to astro
-let Astro = false; // set the nav as closed by default
-function toggleAstro() {
-    if (!Astro) { // if opened is false (ie nav is closed), open the nav
-        dayTime()
-    } else { // else, if opened is ture (ie nav is open), close the nav
-        nightTime();
-    }
-    Astro = !Astro; // negate boolean to get opposite (t to f, and f to t)
-}
+function AstroToggle(){
 
-function dayTime(){
-    console.log("daytime")
-}
-
-function nightTime(){
-    console.log("nighttime")
 }
